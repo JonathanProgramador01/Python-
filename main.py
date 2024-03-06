f49e45b7a0507007ad92ee0f0a3516117290356b
Jonathan Rosas <jonathanroscedprogramador@gmail.com>
2024-03-06 17:02:54 -0600

    Primer ejercicio

--- main.py
+++ main.py
@@ -1,61 +1,16 @@
-import random 
-rock = '''
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-'''
+# Input a list of student scores
+student_scores = input().split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
 
-paper = '''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-'''
+# Your code below this row 👇
+highest_score = 0
+for Students in student_scores:
+  highest_score+=Students
 
-scissors = '''
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
+size=0
+for Students in student_scores:
+  size+=1
 
-#Write your code below this line 👇
-
-Manos=[rock,paper,scissors]
-flag=0
-
-index_ususario=int(input("What do you chosse 0 Rock ,1 paper ,2 Scissors:  "))
-if (index_ususario>=3 or  index_ususario<0):
-  print("Youuu loseeee ")
-  flag=1
-
-if flag!=1:
-  print("TU: ")
-  print(Manos[index_ususario])
-  random_num=random.randint(0,2)
-
-
-  print("Pc: ")
-  print(Manos[random_num]);
-  print("\n\n")
-
-
-
-  if index_ususario==random_num:
-    print("TIED\n\n")
-  elif index_ususario==0 and random_num==2:
-    print("Youu winn")
-  elif index_ususario==2 and random_num==0:
-    print("You loseee ")
-  elif random_num>index_ususario:
-    print("You loseee ")
-  elif index_ususario>random_num:
-    print("Youu winn")
+print(f"TOTALL {highest_score} {size} {highest_score/size}")
 
