5bee6e355b70943f8b9edd22182e3f0de3d70ba4
Jonathan Rosas <jonathanroscedprogramador@gmail.com>
2024-03-06 17:28:22 -0600

    Ejercicio de numeros primos

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-# Input a list of student scores
-student_scores = input().split()
-for n in range(0, len(student_scores)):
-  student_scores[n] = int(student_scores[n])
-
-# Your code below this row 👇
-highest_score = 0
-
-for i in student_scores:
-  if highest_score<i:
-    highest_score=i
-print(f"The highest score in the class is: {highest_score}")
+n=int(input())
+sum=0;
+for i in range(0,n+1):
+    if i%2==0:
+        sum+=i
+print(sum)
