d3f599b3f95224d421090bf2562e51cee4da7051
Jonathan Rosas <jonathanroscedprogramador@gmail.com>
2024-03-06 17:39:33 -0600

    Fizz buss

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-n=int(input())
-sum=0;
-for i in range(0,n+1):
-    if i%2==0:
-        sum+=i
-print(sum)
+n=int (input ())
+
+for i in range(1,n+1):
+    if i%5==0 and i%3==0:
+        print("FizzBuzz")
+    elif i%3==0:
+        print("Fizz")
+    elif i%5==0:
+        print("Buzz")
+    else:
+        print(i)
